613a8784f592435313dcad18f22099e9d304fd64
Zachary Lee <leeyihong03@gmail.com>
2024-04-15 15:50:48 +0800

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -1,27 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
+	// Listen on TCP port 4221 on all interfaces.
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
+	defer l.Close()
+
+	// Accept new connections.
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connection accepted")
+
+	// Create a new reader from the connection
+	reader := bufio.NewReader(conn)
+
+	// Read data from the connection
+	// You can specify your own buffer size or use bufio for simplicity
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from connection:", err.Error())
+			break
+		}
+		fmt.Print("Received: ", line)
+		resp := "HTTP/1.1 200 OK\r\n\r\n"
+		conn.Write([]byte(resp))
+		if err != nil {
+			fmt.Println("Error writing response to connection:", err.Error())
+			break
+		}
 	}
 }
