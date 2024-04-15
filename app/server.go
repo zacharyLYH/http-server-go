39b4a793b6b0c41239c8d9b34a1c0e96cb496fcb
Zac <yihongle@buffalo.edu>
2024-04-16 07:40:20 +0800

    implemented stage 7

--- app/server.go
+++ app/server.go
@@ -52,18 +52,39 @@ func handleConnection(conn net.Conn) {
 	hasPath := requestLines[0]
 	userAgent := "" //stage 5
 	if len(requestLines) > 2 {
+		fmt.Println("Request liens length greater than 2: ", requestLines[2])
 		userAgent = strings.TrimSpace(strings.Split(requestLines[2], " ")[1])
 		fmt.Println(len(userAgent))
 	}
 	path := (strings.Split(hasPath, " "))[1]
 	omitEcho := strings.Split(path, "/echo/") //stage 4
+	var linesRead string 
+	filesPrefix := strings.Split(path, "/files/") //stage 7 
 	resp := ""
 	if path == "/" || len(omitEcho) > 1 || path == "/user-agent" { //stage 3
 		resp = "HTTP/1.1 200 OK\r\n"
+		resp += "Content-Type: text/plain\r\n"
+	} else if len(filesPrefix) > 1 {
+		fmt.Println("File being requested: ", filesPrefix[1])
+		fileName := filesPrefix[1]
+		file, err := os.Open("/tmp/data/codecrafters.io/http-server-tester/"+fileName)
+		if err != nil {
+			resp = "HTTP/1.1 404 NOT FOUND\r\n"
+		}else{
+			defer file.Close()
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				line := scanner.Text()
+				linesRead += line
+			}
+			resp = "HTTP/1.1 200 OK\r\n"
+		}
+		resp += "Content-Type: application/octet-stream\r\n"
 	} else {
 		resp = "HTTP/1.1 404 NOT FOUND\r\n"
+		resp += "Content-Type: text/plain\r\n"
 	}
-	resp += "Content-Type: text/plain\r\n"
+	//Set content-length and return body
 	if path == "/user-agent" {
 		resp += fmt.Sprintf("Content-Length: %d\r\n\r\n", len([]byte(userAgent)))
 		resp += userAgent
@@ -71,6 +92,9 @@ func handleConnection(conn net.Conn) {
 		fmt.Println(omitEcho[1])
 		resp += fmt.Sprintf("Content-Length: %d\r\n\r\n", len([]byte(omitEcho[1])))
 		resp += omitEcho[1]
+	} else if len(filesPrefix) > 1 {
+		resp += fmt.Sprintf("Content-Length: %d\r\n\r\n", len([]byte(linesRead)))
+		resp += linesRead
 	} else {
 		resp += "Content-Length: 0\r\n\r\n"
 	}
